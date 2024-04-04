5a6b59e571c34deb577560ee913d6fe539db257c
Bagas Yudha <bayud.kematian@gmail.com>
2024-04-04 22:45:34 +0700

    first upload

---
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.dicerollerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Nyari Id Button
+        val rollButton: Button = findViewById(R.id.button)
+
+        // Set Click Listener buat spin dadu waktu user klik tombol button
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+     //Fungsi buat roll angka random
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
