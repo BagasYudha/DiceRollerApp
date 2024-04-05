afd1e53f6b060ed77834199f9ca91013fd47e31f
Bagas Yudha <bayud.kematian@gmail.com>
2024-04-05 10:51:38 +0700

    Update variabel

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -11,24 +11,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Nyari Id Button
-        val rollButton: Button = findViewById(R.id.button)
+        val tombolRoll: Button = findViewById(R.id.button)
 
         // Set Click Listener buat spin dadu waktu user klik tombol button
-        rollButton.setOnClickListener { rollDice() }
+        tombolRoll.setOnClickListener { rollDadu() }
     }
 
-    private fun rollDice() {
+    private fun rollDadu() {
         // Create new Dice object with 6 sides and roll it
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
+        val dadu = Dadu(6)
+        val diceRoll = dadu.roll()
 
-        // Update the screen with the dice roll
+        // Update textView sesuai hasil roll Dice
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
 }
 
-class Dice(private val numSides: Int) {
+class Dadu(private val numSides: Int) {
      //Fungsi buat roll angka random
     fun roll(): Int {
         return (1..numSides).random()
