14918cad8ed8a5d4f894fce512cb02ab73e72684
Bagas Yudha <bayud.kematian@gmail.com>
2024-04-05 16:55:07 +0700

    Update and simplification

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dicerollerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -18,19 +19,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDadu() {
-        // Create new Dice object with 6 sides and roll it
-        val dadu = Dadu(6)
-        val diceRoll = dadu.roll()
+        val roll = (1..6).random()
+
+        // Variabel buat roll dadu
+        val rollGambar = when (roll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Variabel buat output imageView dadu
+        val outputDadu: ImageView = findViewById(R.id.imageDadu)
+        outputDadu.setImageResource(rollGambar)
 
         // Update textView sesuai hasil roll Dice
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-    }
-}
-
-class Dadu(private val numSides: Int) {
-     //Fungsi buat roll angka random
-    fun roll(): Int {
-        return (1..numSides).random()
+        val outputText: TextView = findViewById(R.id.textView)
+        outputText.text = "Anda mendapat angka $roll"
     }
 }
